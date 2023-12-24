5ced88abcaf82dc5ba423e6d83c8d3556885f015
Animesh Kumar <animesh.kumar7717@gmail.com>
2023-12-24 20:56:46 +0530

    header: create a header component and style it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='app'> 
+      <Header />
     </div>
   )
 }
