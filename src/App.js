6e014a348ab7424dc0ff8482f53899f9347e426d
Animesh Kumar <animesh.kumar7717@gmail.com>
2023-12-25 14:34:23 +0530

    feat/note: feature develop of note will added.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
 import CreateArea from './components/CreateArea'
+import Note from './components/Note';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
 
-  const addNote = (newNote) => {
-    setNotes(prevNote=>{
-      return [...prevNote, newNote]
-    })
-    console.log('newNote', notes);
+  ;
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
   }
 
   return (
     <div className='app'> 
       <Header />
       <CreateArea onAdd={addNote} />
+      {notes.map((item, index)=>{
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={item.title}
+            content={item.content}
+          />
+        )
+      }
+      )}
     </div>
   )
 }
